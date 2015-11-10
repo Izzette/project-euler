72434f3d1693277ba7866510c0d0d488f6ad4130
Isabell Cowan <isabellcowan@gmail.com>
2015-11-10 11:32:17 -0600

    cleanup on isle euler

--- src/euclid.s
+++ src/euclid.s
@@ -2,126 +2,110 @@
 	.text
 	.global	euclid
 	.type	euclid, @function
-euclid:
-# ui64_t: %rax euclid (
-#	ui64_t a: %rdi,
-#	ui64_t b: %rsi
+# uint64_t euclid (
+#	uint64_t a,
+#	uint64_t b
 # ) {
+euclid:
 .LFB0:
 	pushq	%rbp
 	movq	%rsp,	%rbp
 
-	# if (0 == a)
+	# if (0 == a) {
 	cmpq	$0,	%rdi
-	jne	.LC1
-.LC0:	# {
+	jne	.LC0
 	#	return b;
 	movq	%rsi,	%rax
 	jmp	END
-.LC1:	# }
-	# else if (0 == b)
-	cmpq	$0,	%rdi
-	jne	.LC3
-.LC2:	# {
-	# return a;
-	movq	%rsi,	%rax
-	jmp	END
-.LC3:	# }
+.LC0:	# }
 
-	# ui64_t u, v, t;
+	# if (0 == b) {
+	cmpq	$0,	%rsi
+	jne	.LC1
+	#	return a;
+	movq	%rdi,	%rax
+	jmp	END
+.LC1:	# }
+
 	subq	$24,	%rsp
 	movq	%rdx,	-8(%rbp)
 	movq	%rcx,	-16(%rbp)
 	movq	%rbx,	-24(%rbp)
 
-	# u = a;
-	# v = b;
+	# uint64_t u = a;
 	movq	%rdi,	%rdx
+	# uint64_t v = b;
 	movq	%rsi,	%rcx
-
-	# i = 0; // Was already declared
+	# uint64_t i = 0;
 	movq	$0,	%rax
 
-	# while (
-.LC4:	#	0 == 1 & (u | v)
+	jmp	.LC3
+.LC2:	# {
+	#	u >>= 1;
+	shrq	$1,	%rdx
+	#	v >>= 1;
+	shrq	$1,	%rcx
+	incq	%rax
+.LC3:	# } while (0 == (1 & (u | v)));
 	movq	%rdx,	%rbx
 	orq	%rcx,	%rbx
 	andq	$1,	%rbx
 	cmpq	$0,	%rbx
-	jne	.LC5
-	# ) {
-	# u >>= 1;
-	# v >>= 1;
-	shrq	$1,	%rdx
-	shrq	$1,	%rcx
-	# ++i;
-	incq	%rax
-	jmp	.LC4
-.LC5:	# }
+	je	.LC2
 
-	# while (
-.LC6:	#	0 == (u & 1)
+	jmp	.LC5
+.LC4:	# {
+	#	u >>= 1;
+	shrq	$1,	%rdx
+.LC5:	# } while (0 == (1 & u));
 	movq	%rdx,	%rbx
 	andq	$1,	%rbx
 	cmpq	$0,	%rbx
-	jne	.LC7
-	# ) {
-	# u >>= 1;
-	shrq	$1,	%rdx
-	jmp	.LC6
-.LC7:	# }
+	je	.LC4
 
-.LC10:	# do { 
-	# while (
-	# 	0 == (v & 1)
+.LC6:	# do { 
+	jmp	.LC8
+.LC7:	# {
+	# v >>= 1;
+	shrq	$1,	%rcx
+.LC8:	# } while (0 == (v & 1));
 	movq	%rcx,	%rbx
 	andq	$1,	%rbx
 	cmpq	$0,	%rbx
-	jne	.LC11
-	# ) {
-	# v >>= 1;
-	shrq	$1,	%rcx
-	jmp	.LC10
-.LC11:	# }
+	je	.LC7
 
-	# if (
-	# 	u > v
+	# if (u > v)
 	cmpq	%rdx,	%rcx
-	jge	.LC12
-	# ) {
+	jge	.LC9
+	# {
 	# t = u;
 	movq	%rdx,	%rbx
 	# u = v;
 	movq	%rcx,	%rdx
 	# v = t;
 	movq	%rbx,	%rcx
-.LC12:	# }
+.LC9:	# }
 
 	# v = v - u;
 	subq	%rdx,	%rcx
 
-	# } while (
-	#	0 != v
+	# } while (0 != v);
 	cmpq	$0,	%rcx
-	jne	.LC10
-	addq	$100,	%rsi
+	jne	.LC6
 
-	# while (
-.LC14:	#	0 < i
-	cmpq	$0,	%rax
-	jle	.LC15
-	# ) {
+	jmp	.LC11
+.LC10:	# {
 	# u >>= 1;
 	shlq	$1,	%rdx
 	# --i;
 	decq	%rax
-	jmp	.LC14
-.LC15:	# }
+.LC11:	# } while (0 < i);
+	cmpq	$0,	%rax
+	jg	.LC10
 
-	# ret = u;
+	# return u;
 	movq	%rdx,	%rax
 
-	# free (u, v, t);
 	movq	-8(%rbp), %rdx
 	movq	-16(%rbp), %rcx
 	movq	-24(%rbp), %rbx
